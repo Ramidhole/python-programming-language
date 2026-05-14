756523ba833cfafea5f839d155713c801afe6b2b
Rameshwar Idhole <idholer2@gmail.com>
2026-05-14 10:07:57 +0530

    comment type of and escape sequence

--- basicprogramme.py
+++ basicprogramme.py
@@ -2,3 +2,33 @@ print("Hello world")
 print(5+5)
 print(4/2)
 print("your age is ",30)
+
+#comment in python start with hash
+
+'''
+multiline comments start with triple single quatation and end with single triple quatations
+
+'''
+
+#escape sequence is /n goto next line 
+
+print("my name is ram /" i am from maharashtra/" /n and i am computer science student")
+
+print("Ram is good boy ",   sep="_" , end="...")
+
+a =4
+b="ram"
+c=true
+d= none
+e=6
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(a+e)
+
+print("type of a is"' type(a))
+
+
