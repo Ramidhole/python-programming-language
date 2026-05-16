0ccc5bfba357745760a6d7a58d84c0085c11280d
Rameshwar Idhole <idholer2@gmail.com>
2026-05-17 02:27:26 +0530

    Typecasting

---
+++ typecasting.py
@@ -0,0 +1,34 @@
+Typecasting
+
+Typecasting means converting one data type into another data type.
+
+Types of Typecasting
+1.Implicit Typecasting (Automatic)
+2.Explicit Typecasting (Manual)
+
+1.Implicit Typecasting (Automatic)
+
+Done automatically by the compiler/interpreter.
+Small data type → bigger data type.
+
+example 
+a=5
+b=6.6
+print(a+b)
+output 
+11.6
+
+2.Explicit Typecasting (Manual)
+Done manually by programmer.
+Bigger data type → smaller data type.
+
+example 
+a="5"
+b="4"
+
+print(int(a)+ int(b))
+
+output 
+9
+
+
