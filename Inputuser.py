f5c8bacb88cd1f9dc12e20dcfcc641f297057d5b
Rameshwar Idhole <idholer2@gmail.com>
2026-05-17 07:54:47 +0530

    user input from user

---
+++ Inputuser.py
@@ -0,0 +1,23 @@
+name=input("enter name=")
+print("my name is ", name)
+
+x=int(input("enter number"))
+y=int(input("enter number"))
+print(x+y)
+
+a=input("enter number")
+b=input("enter number")
+print(int(a) + int(b))
+
+a, b = input("Enter two values: ").split()
+print(a, b)
+
+
+a, b = map(int, input().split())
+print(a + b)
+
+
+
+price = float(input("Enter price: "))
+print(price)
+
