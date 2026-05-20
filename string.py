5999b177e672d5ecd3c4965d6cdd8eab5feea013
Rameshwar Idhole <idholer2@gmail.com>
2026-05-20 14:11:45 +0530

    Add string.py with personal introduction and prints

---
+++ string.py
@@ -0,0 +1,14 @@
+name="rameshwar"
+surname='idhole'
+
+description=''' hi my name is ram 
+"i am a software devloper and 
+i am a computer scieence student
+'''
+
+print(name[1])
+print(name[2])
+print(name[3])
+
+print("hi", name)
+
