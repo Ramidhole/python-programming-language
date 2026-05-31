ba8cf27d496a8995e66e5c70fe054c2ddc49114a
Rameshwar Idhole <idholer2@gmail.com>
2026-05-31 13:50:40 +0530

    strings methods

---
+++ Strings_method.py
@@ -0,0 +1,27 @@
+#strings are Immutable 
+a= "!Rameshwar!"
+
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.count("R"))
+print(a.replace("R","s"))
+print(a.rstrip("!"))
+print(a.split(" "))
+print(len(a))
+
+print(a.endswith("!"))
+print(a.find("is"))
+
+print(a.isalnum())
+print(a.isalpha())
+print(a.islower())
+print(a.isupper())
+
+print(a.isprintable())
+print(a.isspace())
+print(a.istitle())
+
+print(a.startwith("r"))
+print(a.swapcase())
+print(a.title())
