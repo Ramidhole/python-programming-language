94fc748915d1484767a1b1cf2ad5c4a6f67017cf
Rameshwar Idhole <idholer2@gmail.com>
2026-06-03 14:41:13 +0530

    check pass or fail by using elif statements

---
+++ check_pass_fail_by_using_elif.py
@@ -0,0 +1,10 @@
+marks = int(input("enter marks:-"))
+
+if(marks>80):
+  print("you are pass your grade is A")
+
+elif(marks<60 && marks>40):
+  print("you are pass your grade is B")
+
+else:
+print("you are fail try again ")
