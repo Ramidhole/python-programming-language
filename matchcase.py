5b7bccaca4910353e95cc87b7027b99ade07a8d4
Rameshwar Idhole <idholer2@gmail.com>
2026-06-12 19:33:43 +0530

    Create matchcase py

---
+++ matchcase.py
@@ -0,0 +1,15 @@
+ptint("enter your order number")
+
+x=int(input("enter number"))
+
+order x:
+
+case 1 : 
+print("you order samosa")
+
+case 2: 
+print("you order milk")
+
+case 3:
+print,("you order tea:)
+
